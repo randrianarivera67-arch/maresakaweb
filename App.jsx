5da808ee83db3e163635bec7f5fc2ef1f4e32ffa
Tony Roch <randrianarivera67@gmail.com>
2026-04-09 15:29:53 +0300

    Tetikasa efa mandeha tsara amin'ny Vite

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Maresaka Web efa mandeha!</h1>
+      <p className="mt-4 text-gray-700">Tafiditra soa aman-tsara ny tetikasanao.</p>
+    </div>
+  );
+}
+
+export default App;
